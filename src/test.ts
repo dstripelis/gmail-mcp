e3ce4e2f11280c4bad8780d31e4c7020c8ca179b
Dimitris Stripelis <dimitris@flower.ai>
2025-09-24 07:46:41 -0700

    adding test script for sending email

---
+++ src/test.ts
@@ -0,0 +1,43 @@
+import {Client} from "@modelcontextprotocol/sdk/client/index.js";
+import {StreamableHTTPClientTransport} from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+import {type CallToolRequest} from "@modelcontextprotocol/sdk/types.js";
+
+// expects to have the MCP Server up and running!
+const streamableClientUrl = new URL("http://localhost:3000/mcp");
+
+// Define RPC messages
+const jsonRpcMessage = {
+  sendEmail: {
+    name: "send_email",
+    arguments: {
+      to: ["dimitris@flower.ai"],
+      subject: "Test Subject",
+      body: "Test Body"
+    }
+  },
+};
+
+async function main() {
+  const client = new Client({
+    name: "gmail-mcp-client",
+    version: "0.0.1"
+  });
+
+  const transport = new StreamableHTTPClientTransport(streamableClientUrl);
+  await client.connect(transport);
+  console.log("✅  Connected to MCP server at", streamableClientUrl.href);
+
+  // Ping
+  const pingResponse = await client.ping();
+  console.log("✅  Ping response:", pingResponse);
+
+  // send a test message
+  const sendEmailParams = jsonRpcMessage.sendEmail as CallToolRequest["params"];  
+  const sendEmailResponse = await client.callTool(sendEmailParams);
+  console.log("✅  Send email content:", sendEmailParams);
+}
+
+main().catch(err => {
+  console.error("❌ Error running MCP client:", err);
+  process.exit(1);
+});
